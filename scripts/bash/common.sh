e2a3b88fe62bb95b302cc26118bd0e74c2905990
TheKoopaKingdom <thekoopakingdom@gmail.com>
2017-11-22 18:01:10 -0500

    Added more color formatters.

--- scripts/bash/common.sh
+++ scripts/bash/common.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 # Only define these once per source.
-BLUE=$(tput setaf 4)
-GREEN=$(tput setaf 2)
-RED=$(tput setaf 1)
 NORMAL=$(tput sgr0)
+# shellcheck disable=SC2034
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+# shellcheck disable=SC2034
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+# shellcheck disable=SC2034
+MAGENTA=$(tput setaf 5)
+# shellcheck disable=SC2034
+CYAN=$(tput setaf 6)
+# shellcheck disable=SC2034
+WHITE=$(tput setaf 7)
+# shellcheck disable=SC2034
+REVERSE=$(tput smso)
+# shellcheck disable=SC2034
+UNDERLINE=$(tput smul)
 
 DEBUG=false
 
