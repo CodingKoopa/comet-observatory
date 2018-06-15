77e852f5bd6ae64124987d57a88aab4018573c24
CodingKoopa <codingkoopa@gmail.com>
2018-06-15 12:41:18 -0400

    Common: Clean up comman variable organization, properly dealt with shellcheck errors.

--- scripts/bash/common.sh
+++ scripts/bash/common.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
-# Only define these once per source.
-NORMAL=$(tput sgr0)
-# shellcheck disable=SC2034
-BLACK=$(tput setaf 0)
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-# shellcheck disable=SC2034
-YELLOW=$(tput setaf 3)
-BLUE=$(tput setaf 4)
-# shellcheck disable=SC2034
-MAGENTA=$(tput setaf 5)
-# shellcheck disable=SC2034
-CYAN=$(tput setaf 6)
-# shellcheck disable=SC2034
-WHITE=$(tput setaf 7)
-# shellcheck disable=SC2034
-REVERSE=$(tput smso)
-# shellcheck disable=SC2034
-UNDERLINE=$(tput smul)
+####################################################################################################
+### State Variables
+####################################################################################################
+export DEBUG=false
 
-DEBUG=false
+####################################################################################################
+### Color Variables
+####################################################################################################
+
+# Only define these once per source.
+NORMAL="$(tput sgr0)"
+readonly NORMAL
+BLACK=$(tput setaf 0)
+readonly BLACK
+RED=$(tput setaf 1)
+readonly RED
+GREEN=$(tput setaf 2)
+readonly GREEN
+YELLOW=$(tput setaf 3)
+readonly YELLOW
+BLUE="$(tput setaf 4)"
+readonly BLUE
+MAGENTA=$(tput setaf 5)
+readonly MAGENTA
+CYAN=$(tput setaf 6)
+readonly CYAN
+WHITE=$(tput setaf 7)
+readonly WHITE
+REVERSE=$(tput smso)
+readonly REVERSE
+UNDERLINE=$(tput smul)
+readonly UNDERLINE
+
 
 debug()
 {
