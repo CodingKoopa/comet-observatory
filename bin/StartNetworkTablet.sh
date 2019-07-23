d52d6973a224692ff4e9953e70ee9d8bf85fcdd0
Koopa <codingkoopa@gmail.com>
2019-07-23 17:45:45 -0400

    Fix networktablet ShellCheck error.

--- bin/StartNetworkTablet.sh
+++ bin/StartNetworkTablet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo echo "Starting GfxTablet, please enter your password:"
-output=$(cat sudo networktablet)
+sudo networktablet
 xinput map-to-output "$( xinput list --id-only "Network Tablet" )" HDMI1
 notify-send "GfxTablet" "GfxTablet is now running."
