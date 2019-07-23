61ece8fcc21533e54539542a059b8a57a0d8157d
Koopa <codingkoopa@gmail.com>
2019-07-23 17:52:23 -0400

    Remove another unused script.

--- bin/add_sd_card_sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ $ACTION = "add" ]]; then
-	su kyle -c 'notify-send "Wii/3DS/Wii U SD card device added, adding MEGA syncronization."'
-	su kyle -c 'mega-sync /run/media/kyle/SD\ CARD /SD\ Card\ Backup'
-
-elif [[ $ACTION = "remove" ]]; then
-	su kyle -c 'notify-send "Wii/3DS/Wii U SD card device removed, adding MEGA syncronization."'
-	su kyle -c 'mega-sync -d "/run/media/kyle/SD CARD"'
-fi
