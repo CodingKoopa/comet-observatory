74d61d9d1edc806ee0276449564e86dce3315eae
Koopa <codingkoopa@gmail.com>
2019-07-23 17:42:36 -0400

    Add preliminary GfxTablet script.

---
+++ bin/StartNetworkTablet.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo echo "Starting GfxTablet, please enter your password:"
+output=$(cat sudo networktablet)
+xinput map-to-output "$( xinput list --id-only "Network Tablet" )" HDMI1
+notify-send "GfxTablet" "GfxTablet is now running."
