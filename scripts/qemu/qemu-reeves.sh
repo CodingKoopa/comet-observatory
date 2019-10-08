119c425a2c5a5d0ae2245cbf146acf3c843b2829
Koopa <codingkoopa@gmail.com>
2019-10-08 18:34:54 -0400

    Add shared directory functionality to QEMU Reeves.

--- scripts/qemu/qemu-reeves.sh
+++ scripts/qemu/qemu-reeves.sh
@@ -81,6 +81,10 @@ function launch-qemu() {
 
   # Block Device Options
 
+  # Add a filesystem device
+  qemu_opts+=" -virtfs local,path=/home/kyle/Library/Virtualization/Share,mount_tag=share,\
+security_model=none"
+
   # USB Options
 
   # Display Options
