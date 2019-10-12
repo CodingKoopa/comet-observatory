af2bcc7781c748713bdda3aad4bd0a7656bd4d27
Koopa <codingkoopa@gmail.com>
2019-10-11 23:11:36 -0400

    Overhaul old download scripts for modernization.

--- scripts/youtube-dl/download_music.sh
+++ scripts/youtube-dl/download_music.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Dependencies:
-#  - "/scripts/bash/common.sh"
-#  - "/scripts/youtube-dl/select_music_directory.sh"
-#  - "/scripts/firefox/music_bookmarks.sh"
-
 # shellcheck source=../bash/common.sh
 source "$COMET_OBSERVATORY/scripts/bash/common.sh"
 # shellcheck source=../youtube-dl/select_music_directory.sh
@@ -15,23 +10,29 @@ source "$COMET_OBSERVATORY/scripts/sqlite3/firefox_music_bookmarks.sh"
 source "$COMET_OBSERVATORY/scripts/eyed3/tag_mp3.sh"
 
 # Downloads music from the Firefox music folder.
+# Outputs:
+#   - Download progress.
 download_music()
 {
-  info "Downloading music from Firefox music bookmark."
-  MUSIC_LIST=$(get_music_list)
+  info "Downloading music from Firefox music bookmark folder."
+  local -ra MUSIC_LIST=$(get_bookmark_urls Music)
   for URL in $MUSIC_LIST; do
     info "Getting info about \"$URL\" and selecting music directory."
-    FILE_PATH=$(select_music_directory "$(youtube-dl --get-title "$URL")")
-    if [ $? -eq 1 ]; then
-      error "An error occured while selecting a music directory."
+    if ! DOWNLOAD_DIR=$(select_music_directory "$(youtube-dl --get-title "$URL")"); then
+      error "Music directory selection failed ($DOWNLOAD_DIR)."
       return 1
     fi
-    info "Downloading to \"$FILE_PATH\"."
-    if ! youtube-dl -o "$FILE_PATH/%(title)s.%(ext)s" -x --audio-format mp3 --embed-thumbnail "$URL"; then
+    info "Downloading to \"$DOWNLOAD_DIR\"."
+    local -r OUTPUT_STR="$DOWNLOAD_DIR/%(title)s.%(ext)s"
+    if ! youtube-dl -q -o "$OUTPUT_STR" -x --audio-format mp3 --embed-thumbnail "$URL"; then
       error "An error occured while downloading the file."
       return 1
     fi
+    # Force MP3 here because otherwise it might return video formats, for YouTube videos.
+    local -r FILE_PATH=$(youtube-dl -o "$DOWNLOAD_DIR/%(title)s.mp3" --get-filename "$URL")
+    info "Tagging metadata."
+    tag_mp3 "$FILE_PATH"
     info "Removing bookmark."
-    remove_bookmark "$URL"
+    remove_firefox_bookmark "$URL"
   done
 }
--- scripts/youtube-dl/select_music_directory.sh
+++ scripts/youtube-dl/select_music_directory.sh
@@ -5,103 +5,110 @@ source "$COMET_OBSERVATORY/scripts/bash/common.sh"
 # shellcheck source=../bash/config.sh
 source "$COMET_OBSERVATORY/scripts/bash/config.sh"
 
-# Makes sure a field isn't empty (This function is really sensitive to changes.).
+# Makes sure a field isn't empty
 # Arguments:
 #  - The input string.
 validate_input()
 {
-  INPUT=$1
+  local -r INPUT=$1
   # If the string is empty/unset, or just consists of a space.
   if [[ -z $INPUT ]]; then
-    echo 1
-  else
     echo 0
+  else
+    echo 1
   fi
 }
 
-# Asks if the directory selection process should be canceled or not.
-prompt_exit()
-{
-  zenity --question --text "The list dialog was closed, or cancel was clicked. Exit?"
-  echo $?
-}
-
-# Selects a music directory using Zenity. Scans category directories in the $MUSIC_DIRECTORY_PATH
+# Selects a music directory using Zenity. Scans category directories in the $MUSIC_DIRECTORY
 # directory, and also subcategories.
 # Arguments:
 #  - The title of the song.
+# Outputs:
+#   - Selection progress.
 select_music_directory()
 {
-  declare -a CATEGORIES
-  declare -a PARENT_CATEGORIES
+  if [[ -z "$MUSIC_DIRECTORY" ]]; then
+    echo "\"MUSIC_DIRECTORY\" variable not set."
+    return 1
+  elif [[ ! -d "$MUSIC_DIRECTORY" ]]; then
+    echo "Music directory \"$MUSIC_DIRECTORY\" not found."
+    return 1
+  fi
+
+  local -a CATEGORIES
+  local -a PARENT_CATEGORIES
   while read -r CATEGORY_DIRECTORY_PATH; do
     # Find will first output the starting point.
-    if [ "$CATEGORY_DIRECTORY_PATH" = "$MUSIC_DIRECTORY_PATH" ]; then
+    if [ "$CATEGORY_DIRECTORY_PATH" = "$MUSIC_DIRECTORY" ]; then
+      continue
+    # Ignore directories left by file synchronization.
+    # .debris   MEGA
+    elif [[ $CATEGORY_DIRECTORY_PATH == *".debris"* ]]; then
       continue
     fi
-    HAS_SUBDIRECTORIES=false
+    local has_subdirectories=false
     # Check if the category has subcategories.
-     while read -r SUBCATEGORY_DIRECTORY_PATH; do
+    while read -r SUBCATEGORY_DIRECTORY_PATH; do
       # Find will first output the starting point, again.
       if [ "$SUBCATEGORY_DIRECTORY_PATH" = "$CATEGORY_DIRECTORY_PATH" ]; then
         continue
-        # Check if the directory is just a disc folder. Regex:
-        # Disc    Match the word Disc.
-        # \ *     Match 0 or more spaces (\ used to escape the space, for Bash.)
-        # [0-9]+  Match a number one or more times.
+      # Check if the directory is just a disc folder. Regex:
+      # Disc    Match the word Disc.
+      # \ *     Match 0 or more spaces (\ used to escape the space, for Bash.)
+      # [0-9]+  Match a number one or more times.
       elif [[ $SUBCATEGORY_DIRECTORY_PATH =~ Disc\ *[0-9]+ ]]; then
         continue
       fi
       debug "Subcategory $SUBCATEGORY_DIRECTORY_PATH."
-      CATEGORIES+=("$( basename "$SUBCATEGORY_DIRECTORY_PATH" )")
-      PARENT_CATEGORIES+=("$( basename "$CATEGORY_DIRECTORY_PATH" )")
-      HAS_SUBDIRECTORIES=true
+      local CATEGORIES+=("$( basename "$SUBCATEGORY_DIRECTORY_PATH" )")
+      local PARENT_CATEGORIES+=("$( basename "$CATEGORY_DIRECTORY_PATH" )")
+      has_subdirectories=true
       # Don't run the while loop in a subshell.
     done <<< "$(find "$CATEGORY_DIRECTORY_PATH" -maxdepth 1 -type d)"
-    if [ $HAS_SUBDIRECTORIES = false ]; then
+    if [ $has_subdirectories = false ]; then
       debug "Category $CATEGORY_DIRECTORY_PATH."
-      CATEGORIES+=("$( basename "$CATEGORY_DIRECTORY_PATH" )")
-      PARENT_CATEGORIES+=("")
+      local CATEGORIES+=("$( basename "$CATEGORY_DIRECTORY_PATH" )")
+      local PARENT_CATEGORIES+=("")
     fi
   # Don't run the while loop in a subshell.
-  done <<< "$(find "$MUSIC_DIRECTORY_PATH" -maxdepth 1 -type d)"
-  declare -a ARGS
+  done <<< "$(find "$MUSIC_DIRECTORY" -maxdepth 1 -type d)"
+  local -a args
   for (( INDEX=0; INDEX<${#CATEGORIES[@]}; ++INDEX )); do
-    ARGS+=(FALSE "${CATEGORIES[INDEX]}" )
+    args+=(FALSE "${CATEGORIES[INDEX]}" )
     if [ "${PARENT_CATEGORIES[INDEX]}" ]; then
-      ARGS+=("${PARENT_CATEGORIES[INDEX]}" )
+      args+=("${PARENT_CATEGORIES[INDEX]}" )
     else
-      ARGS+=(" ")
+      args+=(" ")
     fi
   done
-  NEW_CATEGORY=false
+  local new_category=false
   while true; do
     # TODO: escape &s
-    CATEGORY_INPUT=$(zenity --width 1000 --height 500 \
+    local -r CATEGORY_INPUT=$(zenity --width 1000 --height 500 \
       --list --radiolist \
       --title "Select a Category" \
       --text "Select a category from the list below for the song \"$1\"." \
       --column "" --column "Category Name" --column "Parent Category Name" \
       --print-column 2,3 \
-      TRUE "Make a new category" " " "${ARGS[@]}")
-    CATEGORY_NAME=$(echo "$CATEGORY_INPUT" | cut -d'|' -f1)
-    CATEGORY_NAME_VALID=$(validate_input "$CATEGORY_NAME")
-    PARENT_CATEGORY_NAME=$(echo "$CATEGORY_INPUT" | cut -d'|' -f2)
-    PARENT_CATEGORY_NAME_VALID=$(validate_input "$PARENT_CATEGORY_NAME")
-    debug "Category name: \"$CATEGORY_NAME\". Parent category name: \"$PARENT_CATEGORY_NAME\""
+      TRUE "Make a new category" " " "${args[@]}")
+    local -r CATEGORY_NAME=$(echo "$CATEGORY_INPUT" | cut -d'|' -f1)
+    local -r CATEGORY_NAME_VALID=$(validate_input "$CATEGORY_NAME")
+    local -r PARENT_CATEGORY_NAME=$(echo "$CATEGORY_INPUT" | cut -d'|' -f2)
+    local -r PARENT_CATEGORY_NAME_VALID=$(validate_input "$PARENT_CATEGORY_NAME")
+    # debug "Category name: \"$CATEGORY_NAME\". Parent category name: \"$PARENT_CATEGORY_NAME\""
     # If the user wants to make a new category.
     if [ "$CATEGORY_NAME" = "Make a new category" ]; then
       debug "New category selected."
-      NEW_CATEGORY=true
+      new_category=true
       break
     # If there's a category without a parent.
-    elif [[ $CATEGORY_NAME_VALID -eq 0 ]] && [[ $PARENT_CATEGORY_NAME_VALID -eq 0 ]]; then
+    elif [[ $CATEGORY_NAME_VALID -eq 1 ]] && [[ $PARENT_CATEGORY_NAME_VALID -eq 1 ]]; then
       debug "Category and parent selected."
-      CATEGORY_PATH=$MUSIC_DIRECTORY_PATH$PARENT_CATEGORY_NAME/$CATEGORY_NAME
+      local -r CATEGORY_PATH=$MUSIC_DIRECTORY$PARENT_CATEGORY_NAME/$CATEGORY_NAME
       break
-    elif [[ $CATEGORY_NAME_VALID -eq 0 ]] && [[ $PARENT_CATEGORY_NAME_VALID -eq 1 ]]; then
+    elif [[ $CATEGORY_NAME_VALID -eq 1 ]] && [[ $PARENT_CATEGORY_NAME_VALID -eq 0 ]]; then
       debug "Category selected, not parent."
-      CATEGORY_PATH=$MUSIC_DIRECTORY_PATH$CATEGORY_NAME
+      local -r CATEGORY_PATH=$MUSIC_DIRECTORY$CATEGORY_NAME
       break
     # If, nothing was checked, or cancel was clicked.
     else
@@ -112,32 +119,33 @@ select_music_directory()
         continue
       # If the user does want to exit.
       else
+        echo "Dialog exited."
         return 1
       fi
     fi
   done
 
-  if $NEW_CATEGORY; then
+  if $new_category; then
     debug "Making new category \"$CATEGORY_NAME\""
     while true; do
-      NEW_CATEGORY_INPUT=$(zenity --width 1000 --height 500 \
+      local -ra NEW_CATEGORY_INPUT=$(zenity --width 1000 --height 500 \
       --forms \
       --title "Add a Category" \
       --text "Enter information for the category to put \"$1\" in.". \
       --add-entry "Category Name" \
       --add-entry "Parent Category (Optional)")
-      CATEGORY_NAME=$(echo "$NEW_CATEGORY_INPUT" | cut -d'|' -f1)
-      CATEGORY_NAME_VALID=$(validate_input "$CATEGORY_NAME")
-      PARENT_CATEGORY_NAME=$(echo "$NEW_CATEGORY_INPUT" | cut -d'|' -f2)
-      PARENT_CATEGORY_NAME_VALID=$(validate_input "$PARENT_CATEGORY_NAME")
+      local -r CATEGORY_NAME=$(echo "$NEW_CATEGORY_INPUT" | cut -d'|' -f1)
+      local -r CATEGORY_NAME_VALID=$(validate_input "$CATEGORY_NAME")
+      local -r PARENT_CATEGORY_NAME=$(echo "$NEW_CATEGORY_INPUT" | cut -d'|' -f2)
+      local -r PARENT_CATEGORY_NAME_VALID=$(validate_input "$PARENT_CATEGORY_NAME")
       debug "Category name: \"$CATEGORY_NAME\"$CATEGORY_NAME_VALID. Parent category name: \"$PARENT_CATEGORY_NAME\""
-      if [[ $CATEGORY_NAME_VALID -eq 0 ]] && [[ $PARENT_CATEGORY_NAME_VALID -eq 0 ]]; then
-        debug "Category and parent exist."
-        CATEGORY_PATH=$MUSIC_DIRECTORY_PATH$PARENT_CATEGORY_NAME/$CATEGORY_NAME
+      if [[ $CATEGORY_NAME_VALID -eq 1 ]] && [[ $PARENT_CATEGORY_NAME_VALID -eq 1 ]]; then
+        # debug "Category and parent exist."
+        local -r CATEGORY_PATH=$MUSIC_DIRECTORY$PARENT_CATEGORY_NAME/$CATEGORY_NAME
         break
-      elif [[ $CATEGORY_NAME_VALID -eq 0 ]] && [[ $PARENT_CATEGORY_NAME_VALID -eq 1 ]]; then
-        debug "Category exists, no parent."
-        CATEGORY_PATH=$MUSIC_DIRECTORY_PATH$CATEGORY_NAME
+      elif [[ $CATEGORY_NAME_VALID -eq 1 ]] && [[ $PARENT_CATEGORY_NAME_VALID -eq 0 ]]; then
+        # debug "Category exists, no parent."
+        local -r CATEGORY_PATH=$MUSIC_DIRECTORY$CATEGORY_NAME
         break
       else
         # Prompt for exit.
@@ -155,12 +163,12 @@ select_music_directory()
   fi
 
   if [ -z "$CATEGORY_PATH" ]; then
-    error "Category path not set."
+    echo "Category path not set."
     return 1
   fi
   debug "Category path: \"$CATEGORY_PATH\"."
   if [ ! -d "$CATEGORY_PATH" ]; then
-    error "Category path not found."
+    echo "Category path not found."
     return 1
   fi
   echo "$CATEGORY_PATH"
