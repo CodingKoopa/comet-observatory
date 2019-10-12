8f717dbd1813d203139b37a38e9fc61d99f9ca19
Koopa <codingkoopa@gmail.com>
2019-10-11 22:57:04 -0400

    Add revamped Bash RC file.

---
+++ scripts/bash/bash-rc.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+# Export environmental variables.
+
+# Set the Comet Observatory path.
+export COMET_OBSERVATORY=$HOME/Documents/Projects/Bash/comet-observatory
+
+# Set the PATHt to prioritize ccache, and deprioritize local binaries and secondary toolchains.
+export PATH=/usr/lib/ccache/bin/:$PATH:$COMET_OBSERVATORY/bin:/home/kyle/frc2019/roborio/bin
+
+# Set the editor to nano.
+export EDITOR=nano
+
+# Disable the Wine menu builder, to prevent creation of desktop entries for Windows programs, Mono,
+# to prevent the Mono installation dialog, and Gecko, to prevent the Gecko installation dialog.
+export WINEDLLOVERRIDES="winemenubuilder.exe=d;mscoree=d;mshtml=d"
+
+# Execute initialization scripts.
+
+# Export FRC toolchain variables.
+# $HOME/frc2019/frccode/frcvars2019.sh
+
+# Set functions and aliases.
+
+# Reloads this script.
+# Globals Exported:
+#   - Everything exported in this script.
+function r
+{
+  clear
+  # shellcheck source=./bash-rc.sh
+  source ~/.bashrc
+}
+
+# Exports the contents of an "ENV" file.
+# Arguments:
+#   - The path to the "ENV" file.
+function export-env
+{
+  export "$(grep -v '^#' "$1" | xargs)"
+}
+
+# Executes "ls", with color on.
+alias ls='ls --color=auto'
+
+# Customize the prompt.
+
+QUOTES=(
+  # Neon Genesis Evangelion.
+  "Understanding 100% of everything is impossible. That’s why we spend all our lives trying to \
+understand the thinking of others. That’s what makes life so interesting."
+  "Sometimes you need a little wishful thinking just to keep on living."
+  "Part of growing up means finding a way to interact with others while distancing pain."
+  "Never underestimate the ability of the human animal to adapt to its environment."
+  "Any new position from which you view your reality will change your perception of its nature. \
+It's all literally a matter of perspective."
+  "Your truth can be changed simply by the way you accept it. That's how fragile the truth for a \
+human is."
+  "Anywhere can be paradise as long as you have the will to live. After all, you are alive, so you \
+will always have the chance to be happy. As long as the Sun, the Moon, and the Earth exist, \
+everything will be all right."
+  "Humans constantly feel pain in their hearts. Because the heart is so sensitive to pain, humans \
+also feel that to live is to suffer."
+  "I'm so fucked up."
+  "Kimochi warui."
+  # Mob Psycho 100
+  "The biggest distinguishing feature of humans is how rich and varied their emotions are. These \
+emotions are the reason they thrive and also the reason they fight." \
+  "You should realize that you're only able to survive thanks to the help of others."
+)
+echo "${QUOTES[$RANDOM % ${#QUOTES[@]}]}" | lolcat -F 0.01
+
+# shellcheck source=../../externals/agnoster-bash/agnoster.bash
+source "$COMET_OBSERVATORY/externals/agnoster-bash/agnoster.bash"
