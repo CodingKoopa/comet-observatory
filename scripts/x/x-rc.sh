8d072c3f12d83b23026dd675746857bd63853d32
Koopa <codingkoopa@gmail.com>
2019-10-11 23:14:18 -0400

    Add xinit script.

---
+++ scripts/x/x-rc.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Source system xinitrc scripts.
+if [ -d /etc/X11/xinit/xinitrc.d ] ; then
+ for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
+  # shellcheck disable=1090
+  [ -x "$f" ] && . "$f"
+ done
+ unset f
+fi
+
+# Disable Energy Star features.
+xset -dpms
+# Disable screen blanking.
+xset s off
+
+# Use the XDG portal for GTK applications. This allows for things like Firefox (GTK) using KDE file
+# dialogs.
+export GTK_USE_PORTAL=1
+
+# Start KDE Plasma.
+exec startkde
