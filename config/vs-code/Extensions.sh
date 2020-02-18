18f55873ff0e8393b26e6cb65ad77b3e15a4115e
Koopa <codingkoopa@gmail.com>
2020-02-17 20:47:58 -0500

    Add VSCode extensions.

---
+++ config/vs-code/Extensions.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Generated with "code --list-extensions | xargs -L 1 echo code --install-extension", from
+# https://stackoverflow.com/a/49398449.
+
+code --install-extension ban.spellright
+code --install-extension bungcip.better-toml
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension fatihacet.gitlab-workflow
+code --install-extension geddski.macros
+code --install-extension GitHub.vscode-pull-request-github
+code --install-extension icrawl.discord-vscode
+code --install-extension mikestead.dotenv
+code --install-extension mrmlnc.vscode-csscomb
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools
+code --install-extension redhat.java
+code --install-extension redhat.vscode-yaml
+code --install-extension reignofwebber.c-cpp-definition-generator
+code --install-extension timonwong.shellcheck
+code --install-extension twxs.cmake
+code --install-extension vscjava.vscode-java-debug
+code --install-extension vscjava.vscode-java-dependency
+code --install-extension wpilibsuite.vscode-wpilib
+code --install-extension yzhang.markdown-all-in-one
