ea1a7ec7c5b29d481de8692c1f2e380f184e2ba5
Koopa <codingkoopa@gmail.com>
2020-04-06 08:32:17 -0400

    Don't discard startx logging.

--- scripts/bash/bash_profile.sh
+++ scripts/bash/bash_profile.sh
@@ -7,5 +7,5 @@
 # If the systemd graphical target unit is active, the display isn't setup, and the first virtual
 # console is active, start X.
 if systemctl -q is-active graphical.target && [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
-  exec startx &> /dev/null
+  startx -- -keeptty &> "$HOME/.local/share/xorg/startx.log"
 fi
