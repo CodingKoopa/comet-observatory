17751a5aac63fcb17990488ef44bec32fcdbd390
Koopa <codingkoopa@gmail.com>
2020-05-26 14:56:56 -0400

    Move DE launch variables to PAM environment.
    before-de-launch's variables weren't actually being exported, for some
    reason.

--- scripts/x/x_rc.sh
+++ scripts/x/x_rc.sh
@@ -13,8 +13,5 @@ if [[ -d /etc/X11/xinit/xinitrc.d ]]; then
   unset f
 fi
 
-# Sets environment variables for graphical applications. See /docs/Init.md.
-before-de-launch
-
 # Start KDE Plasma.
 exec startplasma-x11
