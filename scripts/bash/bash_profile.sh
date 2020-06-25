041167593be37c3bbfb9e560fdf237a46d3bb91c
Koopa <codingkoopa@gmail.com>
2020-06-25 17:12:18 -0400

    Disable startx logging.
    This hasn't really been a useful thing to have.

--- scripts/bash/bash_profile.sh
+++ scripts/bash/bash_profile.sh
@@ -7,7 +7,7 @@
 # If the systemd graphical target unit is active, the display isn't setup, and the first virtual
 # console is active, start X. See /docs/Init.md.
 if systemctl -q is-active graphical.target && [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
-  startx -- -keeptty &>"$HOME/.local/share/xorg/startx.log"
+  exec startx
 else
   # shellcheck source=scripts/bash/bash_rc.sh
   "$CO"/scripts/bash/bash_rc.sh
