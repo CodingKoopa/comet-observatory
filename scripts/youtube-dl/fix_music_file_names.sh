317eb9f5757ae77fe7e633bff5b3fc03defefe66
Koopa <codingkoopa@gmail.com>
2020-06-25 16:36:38 -0400

    Fix readonly variables.

--- scripts/youtube-dl/fix_music_file_names.sh
+++ scripts/youtube-dl/fix_music_file_names.sh
@@ -22,8 +22,10 @@ function fix_music_file_names() {
 
   # TODO: soundcloud downloads
   find "$MUSIC_DIRECTORY/" -type f -name '*.mp3' -o -name '*.wav' | while read -r file_path; do
-    local -r file_name=$(basename "$file_path")
-    local -r file_directory=$(dirname "$file_path")
+    local file_name
+    file_name=$(basename "$file_path")
+    local file_directory
+    file_directory=$(dirname "$file_path")
     # -[-|0-9|A-Z|_|a-z]{11}\.(mp3|wav)$ is the regex used for detecting music downloaded with the
     # default youtube-dl settings, with the video ID in its name.
     # -                   The dash yotube-dl inserts to divide the video title from the video ID.
