3adc048e1e7f71b230201c5525869caaf18c6c6a
Koopa <codingkoopa@gmail.com>
2020-06-25 17:17:48 -0400

    Clean up download_music.

--- scripts/youtube-dl/download_music.sh
+++ scripts/youtube-dl/download_music.sh
@@ -18,23 +18,28 @@ source "$CO"/scripts/eyed3/tag_mp3.sh
 #   - Download progress.
 function download_music() {
   info "Downloading music from Firefox music bookmark folder."
-  for URL in $(get_bookmark_urls "Listening List"); do
-    info "Getting info about \"$URL\" and selecting music directory."
-    if ! download_dir=$(select_music_directory "$(youtube-dl --get-title "$URL")"); then
-      error "Music directory selection failed ($download_dir)."
-      return 1
-    fi
-    info "Downloading to \"$download_dir\"."
-    local -r output_str="$download_dir/%(title)s.%(ext)s"
-    if ! youtube-dl -q -o "$output_str" -x --audio-format mp3 --embed-thumbnail "$URL"; then
-      error "An error occured while downloading the file."
-      return 1
-    fi
-    # Force MP3 here because otherwise it might return video formats, for YouTube videos.
-    local -r FILE_PATH=$(youtube-dl -o "$download_dir/%(title)s.mp3" --get-filename "$URL")
-    info "Tagging metadata."
-    tag_mp3 "$FILE_PATH"
-    info "Removing bookmark."
-    remove_firefox_bookmark "$URL"
-  done
+  if urls=$(get_bookmark_urls "Listening List"); then
+    for url in $urls; do
+      info "Getting info about \"$url\" and selecting music directory."
+      if ! download_dir=$(select_music_directory "$(youtube-dl --get-title "$url")"); then
+        error "Music directory selection failed ($download_dir)."
+        return 1
+      fi
+      info "Downloading to \"$download_dir\"."
+      local -r output_str="$download_dir/%(title)s.%(ext)s"
+      if ! youtube-dl -q -o "$output_str" -x --audio-format mp3 --embed-thumbnail "$url"; then
+        error "An error occured while downloading the file."
+        return 1
+      fi
+      # Force MP3 here because otherwise it might return video formats, for YouTube videos.
+      local -r FILE_PATH=$(youtube-dl -o "$download_dir/%(title)s.mp3" --get-filename "$url")
+      info "Tagging metadata."
+      # TODO: error handling here
+      tag_mp3 "$FILE_PATH"
+      info "Removing bookmark."
+      remove_firefox_bookmark "$url"
+    done
+  else
+    error "$urls"
+  fi
 }
