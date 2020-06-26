d5a8c2cfcb22801bdfb060ba079b92b8fe36584f
Koopa <codingkoopa@gmail.com>
2020-06-25 22:56:39 -0400

    Fix tkgmark line length.

--- scripts/makepkg/tkgmark.sh
+++ scripts/makepkg/tkgmark.sh
@@ -15,5 +15,6 @@ source "$CO"/scripts/makepkg/repos.sh
 function tkgmark() {
   # shellcheck source=config/tkg/linux57-tkg.cfg
   source "$CO"/config/tkg/linux$KERNEL_VER-tkg.cfg
-  echo "$_cpusched, $_sched_yield_type, $_rr_interval, $_tickless, $_voluntary_preempt, ${_irq_threading:-"N/A"}, ${_smt_nice:-"N/A"}, ${_runqueue_sharing:-"N/A"}, $_timer_freq"
+  echo "$_cpusched, $_sched_yield_type, $_rr_interval, $_tickless, $_voluntary_preempt, \
+${_irq_threading:-"N/A"}, ${_smt_nice:-"N/A"}, ${_runqueue_sharing:-"N/A"}, $_timer_freq"
 }
