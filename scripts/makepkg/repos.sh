2056e5bd771d7d87d44444ea0fc8582eff7cfaca
Koopa <codingkoopa@gmail.com>
2020-07-16 19:42:40 -0400

    Alwyas open the configuration for nvidia-all.

--- scripts/makepkg/repos.sh
+++ scripts/makepkg/repos.sh
@@ -81,7 +81,10 @@ function update_repo() {
   fi
 
   if [[ ${#diff_files[@]} -ne 0 ]]; then
-    if [[ -n $(git diff "@{1}.." "${diff_files[@]}") ]]; then
+    # Only view the configuration and configuration diff if there has been a change to it. In the
+    # case of nvidia-all, one may want to update the version, but new versions do not necessarily
+    # cause version diffs, so force it.
+    if [[ -n $(git diff "@{1}.." "${diff_files[@]}") || $repo = "nvidia-all" ]]; then
       local cfg_name=$repo_base
       if [[ $repo == "dxvk-tools" ]]; then
         cfg_name="updxvk"
