702bf21f8bf17773282cad9e71e4fbc1bba51ad1
Koopa <codingkoopa@gmail.com>
2020-08-10 13:09:27 -0400

    Update VSCode extensions.

--- config/vs-code/Extensions.sh
+++ config/vs-code/Extensions.sh
@@ -3,18 +3,17 @@
 # Generated with "code --list-extensions | xargs -L 1 echo code --install-extension", from
 # https://stackoverflow.com/a/49398449.
 
-code --install-extension ban.spellright
 code --install-extension bungcip.better-toml
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension EditorConfig.EditorConfig
-code --install-extension fatihacet.gitlab-workflow
 code --install-extension foxundermoon.shell-format
 code --install-extension geddski.macros
-code --install-extension GitHub.vscode-pull-request-github
-code --install-extension icrawl.discord-vscode
+code --install-extension GitLab.gitlab-workflow
+code --install-extension James-Yu.latex-workshop
 code --install-extension mikestead.dotenv
 code --install-extension mrmlnc.vscode-csscomb
 code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.python
 code --install-extension ms-vscode.cmake-tools
 code --install-extension ms-vscode.cpptools
 code --install-extension redhat.java
