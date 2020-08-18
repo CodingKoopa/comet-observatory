a830ca568a5f2a8c249e014eff7bf2a93299b39c
Koopa <codingkoopa@gmail.com>
2020-08-18 16:25:14 -0400

    Undo XZ deletion.
    This actually results in redownloading files more than I would like.

--- scripts/makepkg/repos.sh
+++ scripts/makepkg/repos.sh
@@ -126,7 +126,7 @@ function build_repo() {
   safe_cd "$AUR_DIR/$repo"
 
   verbose "Cleaning old packages and build logs."
-  rm -f ./*.xz ./*.pkg.* ./*.log
+  rm -f ./*.pkg.* ./*.log
   verbose "Building."
   makepkg -si --noconfirm
 
