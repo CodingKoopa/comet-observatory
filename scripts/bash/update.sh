4fcbc574e6107a35c38b6eca09406e4c4d8799fa
Koopa <codingkoopa@gmail.com>
2020-08-23 21:16:40 -0400

    Use package ignore list.

--- scripts/bash/update.sh
+++ scripts/bash/update.sh
@@ -120,12 +120,7 @@ function update() {
     section "Updating Prebuilt Packages"
 
     subsect "Syncing official and Chaotic AUR packages."
-    pikaur -Syu --noconfirm --ignore linux$KERNEL_VER-tkg-pds,linux$KERNEL_VER-tkg-pds-headers \
-      --ignore nvidia-dev-dkms-tkg,nvidia-dev-utils-tkg,lib32-nvidia-dev-utils-tkg \
-      --ignore nvidia-dev-settings-tkg,nvidia-dev-egl-wayland-tkg \
-      --ignore opencl-nvidia-dev-tkg,lib32-opencl-nvidia-dev-tkg \
-      --ignore mingw-w64-binutils --ignore mingw-w64-binutils \
-      --ignore proton-tkg-git --ignore vkd3d-tkg-git
+    pikaur -Syu --noconfirm "${PACKAGE_IGNORE_ARGS[@]}"
   fi
   if [[ $run_fstrim = true ]]; then
     section "Running fstrim"
