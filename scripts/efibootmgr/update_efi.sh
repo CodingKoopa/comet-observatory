f48de8ebfc1e38a9e028e2b72fd5203d607fcf8d
Koopa <codingkoopa@gmail.com>
2020-08-26 11:17:38 -0400

    Add tkgmatrix support to update-efi.

--- scripts/efibootmgr/update_efi.sh
+++ scripts/efibootmgr/update_efi.sh
@@ -161,8 +161,8 @@ $CMDLINE_STR $*"
   # Iterate over the TkG kernel initramfs images, in order from oldest to most recent.
   # shellcheck disable=2044
   for TKG_VMLINUZ_PATH in $(find /boot -mindepth 1 -maxdepth 1 -type f \
-    -regex '/boot/vmlinuz-linux[0-9]+-tkg.*' | sort -n); do
-    tkg_version=$(echo "$TKG_VMLINUZ_PATH" | grep -oh "[0-9].")
+    -regex '/boot/vmlinuz-linux\([0-9]+-tkg\|-tkgmatrix\).*' | sort -n); do
+    tkg_version=$(echo "$TKG_VMLINUZ_PATH" | grep -oh "[0-9]." || echo "Matrix")
     current_tkg_name="TkG $(echo "$tkg_version" | cut -c 1).$(echo "$tkg_version" | cut -c 2-)"
     kernel_suffixes[$current_tkg_name]=${TKG_VMLINUZ_PATH#/boot/vmlinuz-linux}
   done
