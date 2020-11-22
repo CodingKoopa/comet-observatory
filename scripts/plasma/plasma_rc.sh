b43cfc9633d98f122608f061afd875c811cbaa82
Koopa <codingkoopa@gmail.com>
2020-11-22 11:47:53 -0500

    Disable DPMS on DE launch.

---
+++ scripts/plasma/plasma_rc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Copyright 2020 Comet Observatory
+# Licensed under GPLv3.
+# Refer to License.txt file included.
+
+# Disable DPMS and prevent screen from blanking.
+xset s off -dpms
