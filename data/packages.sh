22bf72154787ebb489b207ef97becf91cb89ffa3
Koopa <codingkoopa@gmail.com>
2021-08-22 10:58:14 -0400

    Do install Xorg.

--- data/packages.sh
+++ data/packages.sh
@@ -155,6 +155,7 @@ lolcat
 
 # xinit, for logging in.
 xorg-xinit
+xorg-server
 # xmousepasteblock, for preventing accidental mouse button press pastes with Xorg.
 xmousepasteblock-git
 # xsel, for clipboard support for tag_mp3.sh.
