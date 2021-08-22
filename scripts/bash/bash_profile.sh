d679f8c42ef919c9a721b189f2d96ac56640fea0
Koopa <codingkoopa@gmail.com>
2021-08-22 10:56:12 -0400

    Source bash_rc unconditionally.

--- scripts/bash/bash_profile.sh
+++ scripts/bash/bash_profile.sh
@@ -8,11 +8,11 @@ export CO=$HOME/Documents/Projects/Bash/comet-observatory
 # shellcheck source=scripts/bash/user_rc.sh
 source "$CO"/scripts/bash/user_rc.sh
 
+# shellcheck source=scripts/bash/bash_rc.sh
+"$CO"/scripts/bash/bash_rc.sh
+
 # If the systemd graphical target unit is active, the display isn't setup, and the first virtual
 # console is active, start X. See /docs/Init.md.
 if systemctl -q is-active graphical.target && [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
   exec startx -- vt1 &>/dev/null
-else
-  # shellcheck source=scripts/bash/bash_rc.sh
-  "$CO"/scripts/bash/bash_rc.sh
 fi
