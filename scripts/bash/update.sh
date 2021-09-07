99a4892af32290b7ab82f975f05dcb28f72b722f
Koopa <codingkoopa@gmail.com>
2021-09-07 02:13:46 -0400

    update: organize and clean script

--- scripts/bash/update.sh
+++ scripts/bash/update.sh
@@ -11,26 +11,30 @@ source "$CO"/scripts/bash/common.sh
 # shellcheck source=scripts/makepkg/repos.sh
 source "$CO"/scripts/makepkg/repos.sh
 
+readonly OPTIONS=hapftsome
+
 # Prints the usage info for this script.
 # Outputs:
 #   - The help message.
 function print_help() {
   # Keep the help string in its own variable because a single quote in a heredoc messes up syntax
   # highlighting.
-  HELP_STRING="\
-Usage: update [-hpc]
+  readonly HELP_STRING="\
+Usage: update [-$OPTIONS]
 Updates the system, including official prebuilt packages, AUR packages, and, locally patched
-custom built packages. Also handles some routine maintenance tasks.
+custom built packages.
 
   -h    Show this help message and exit.
   -a    Do everything. This is the default behavior.
-  -s    Review and update suckless packages.
-  -f    Force update all custom packages even if the repo is up to date. Not done by -a.
-  -t    Review and update TkG packages.
-  -p    Update prebuilt packages. This essentially runs pacman -Syu for official and AUR packages.
-  -e    Check for extraneous packages. They won't necessarily be removed.
-  -o    Remove orphan packages. Not included with -a because, generally, we will be keeping build
-dependencies installed, which are considered orphaned packages."
+  -p    Review and update prebuilt packages.
+  -f    Force update all custom packages even if the repo is up to date. Implies -s and -t. Not
+  included by -a.
+  -t    Review and update custom TkG packages. Not included by -a.
+  -s    Review and update custom suckless packages.
+  -o    Remove orphan packages. Not included by -a because, generally, we will be keeping build
+  dependencies installed, which are considered orphaned packages.
+  -m    Check for missing packages. They won't be addded.
+  -e    Check for extraneous packages. They won't be removed."
   echo "$HELP_STRING"
   exit 0
 }
@@ -43,11 +47,12 @@ dependencies installed, which are considered orphaned packages."
 # Outputs:
 #   - Update progress.
 function update() {
+  local update_prebuilt=false
+  local update_tkg=false
+  local update_suckless=false
+  local force_custom=false
   local check_missing=false
   local check_extra=false
-  local update_tkg=false
-  local force_custom=false
-  local update_prebuilt=false
   local remove_orphans=false
 
   if [[ $# -eq 0 ]]; then
@@ -63,20 +68,28 @@ function update() {
       check_extra=true
       update_prebuilt=true
       ;;
-    t)
-      update_tkg=true
-      ;;
-    f)
-      force_custom=true
-      ;;
     p)
       update_prebuilt=true
       ;;
-    e)
-      check_extra=true
+    f)
+      force_custom=true
+      update_tkg=true
+      update_suckless=true
+      ;;
+    t)
+      update_tkg=true
+      ;;
+    s)
+      update_suckless=true
       ;;
     o)
       remove_orphans=true
+      ;;
+    m)
+      check_missing=true
+      ;;
+    e)
+      check_extra=true
       ;;
     *)
       print_help
@@ -87,36 +100,6 @@ function update() {
   # Ask for the sudo password now so that the rest of the process can proceed without it.
   sudo echo >/dev/null
 
-  function handle_pacnew() {
-    # Handle any pacnew/pacsave files issues.
-    sudo -E DIFFPROG="sudo code -d" pacdiff
-  }
-
-  if [[ $remove_orphans = true ]]; then
-    section "Removing Orphan Packages"
-
-    pikaur -Qtdq | pikaur -Rns -
-  fi
-  if [[ $check_missing = true ]]; then
-    section "Checking for Missing Packages"
-
-    diff=$(comm -13 <(get_installed_package_list) <(get_co_package_list))
-    if [[ -n $diff ]]; then
-      info "These packages are on the CO list, but aren't installed on the system:"
-      echo "$diff"
-      info "Considering installing them or updating the list."
-    fi
-  fi
-  if [[ $check_extra = true ]]; then
-    section "Checking for Extraneous Packages"
-
-    diff=$(comm -23 <(get_installed_package_list) <(get_co_package_list))
-    if [[ -n $diff ]]; then
-      info "These packages are explicitly installed on the system, but aren't on the CO list:"
-      echo "$diff"
-      info "Considering uninstalling them or adding them to the list."
-    fi
-  fi
   if [[ $update_prebuilt = true ]]; then
     section "Updating Prebuilt Packages"
 
@@ -166,6 +149,31 @@ function update() {
       build_repo wine-tkg-git/proton-tkg
     else
       info "Already up to date."
+    fi
+  fi
+  if [[ $remove_orphans = true ]]; then
+    section "Removing Orphan Packages"
+
+    pikaur -Qtdq | pikaur -Rns -
+  fi
+  if [[ $check_missing = true ]]; then
+    section "Checking for Missing Packages"
+
+    diff=$(comm -13 <(get_installed_package_list) <(get_co_package_list))
+    if [[ -n $diff ]]; then
+      info "These packages are on the CO list, but aren't installed on the system:"
+      echo "$diff"
+      info "Considering installing them or updating the list."
+    fi
+  fi
+  if [[ $check_extra = true ]]; then
+    section "Checking for Extraneous Packages"
+
+    diff=$(comm -23 <(get_installed_package_list) <(get_co_package_list))
+    if [[ -n $diff ]]; then
+      info "These packages are explicitly installed on the system, but aren't on the CO list:"
+      echo "$diff"
+      info "Considering uninstalling them or adding them to the list."
     fi
   fi
 }
