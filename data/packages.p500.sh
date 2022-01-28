67006b6f7071b3bbaccc1e32feb83ced5704637f
Koopa <codingkoopa@gmail.com>
2022-01-28 17:26:26 -0500

    packages: add mesa for p500

--- data/packages.p500.sh
+++ data/packages.p500.sh
@@ -4,6 +4,9 @@
 
 # Intel Microcode, for CPU patches.
 intel-ucode
+# Mesa OpenGL implementation; part of the DRI.
+mesa
+lib32-mesa
 # Vulkan driver for Intel graphics.
 vulkan-intel
 # 32-bit Vulkan driver for Intel graphics.
