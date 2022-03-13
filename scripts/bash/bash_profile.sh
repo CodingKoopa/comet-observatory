c72f3337073ab82fc36ec3afda8cdf2ed6c13ae4
Koopa <lucy.loerker@gmail.com>
2022-03-13 19:14:31 -0400

    bash_profile: add startx logging

--- scripts/bash/bash_profile.sh
+++ scripts/bash/bash_profile.sh
@@ -13,7 +13,7 @@ source "$CO"/scripts/bash/user_rc.sh
 # If the systemd graphical target unit is active, the display isn't setup, and the first virtual
 # console is active, start X. See /docs/Init.md.
 if systemctl -q is-active graphical.target && [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then
-  exec startx -- vt1 &>/dev/null
+  exec startx -- vt1 &>"$HOME"/.local/share/startx.log
 else
   # shellcheck source=scripts/bash/bash_rc.sh
   "$CO"/scripts/bash/bash_rc.sh
