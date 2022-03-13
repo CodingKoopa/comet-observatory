82208e326e446a8e14721d516361ac20d65f71d8
Koopa <lucy.loerker@gmail.com>
2022-03-13 19:51:53 -0400

    bluetooth: converge config
    I'm finding that having BT enabled all the time actually is appropriate
    for the laptop.

--- scripts/bash/configure_system_utils.sh
+++ scripts/bash/configure_system_utils.sh
@@ -96,11 +96,9 @@ function configure_system_units() {
 
   safe_cp "$CO"/config/journald.conf /etc/systemd/journald.conf
   safe_cp "$CO"/config/httpd.conf /etc/httpd/conf/httpd.conf
+  safe_cp "$CO"/config/bluetooth.conf /etc/bluetooth/main.conf
   if [[ $CO_HOST = "DESKTOP" ]]; then
     safe_cp "$CO"/config/pulse-daemon.conf /etc/pulse/daemon.conf
-    safe_cp "$CO"/config/bluetooth.desktop.conf /etc/bluetooth/main.conf
-  elif [[ $CO_HOST = "LAPTOP_P500" ]]; then
-    safe_cp "$CO"/config/bluetooth.p500.conf /etc/bluetooth/main.conf
   fi
 }
 
