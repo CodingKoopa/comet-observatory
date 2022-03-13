9e14757e0ee30c49728bc03f0decad6e01c8463f
Koopa <lucy.loerker@gmail.com>
2022-03-13 01:53:13 -0500

    co_rc: check for CO existence

--- scripts/bash/co_rc.sh
+++ scripts/bash/co_rc.sh
@@ -3,6 +3,10 @@
 # Set the path to the Comet Observatory.
 export CO=/home/koopa/Documents/Projects/Bash/comet-observatory
 
+if [[ ! -d $CO ]]; then
+  printf 'Error: CO doesn'\''t exist at "%s". Expect dragons.' $CO >&2
+fi
+
 # Set the host.
 product_version=$(cat /sys/devices/virtual/dmi/id/product_version)
 readonly product_version
