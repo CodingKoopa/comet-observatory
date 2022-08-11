e3bc4ef1da066df5b20bffa86ce093260c757162
Koopa <codingkoopa@gmail.com>
2022-08-11 16:00:04 -0400

    firefox: switch to smooth scrolling V4

--- config/firefox.js
+++ config/firefox.js
@@ -25,72 +25,54 @@ user_pref("media.ffmpeg.vaapi.enabled", true);
 user_pref("gfx.webrender.all", true);
 
 
-////  NATURAL SMOOTH SCROLLING
-////  https://pastebin.com/y5NvtjmD
-// preset             info               [default]
-// NSS     5        ACCEL MAX SPEED 1-20           [10]
-user_pref("mousewheel.acceleration.factor", 5);
-// NSS     4        accel after x ticks            [-1]
-user_pref("mousewheel.acceleration.start", 4);
-// NSS   100           reset previous             [100]
-user_pref("mousewheel.default.delta_multiplier_x", 100);
-// NSS   100           reset previous             [100]
-// Empiraclly determined to make each "scroll" the same as Chrome.
-user_pref("mousewheel.default.delta_multiplier_y", 77);
-// NSS   100           reset previous             [100]
-user_pref("mousewheel.default.delta_multiplier_z", 100);
-// NSS  false       ignoring sys accel           [true]
-user_pref("mousewheel.system_scroll_override_on_root_content.enabled", false);
-// NSS  1500           reset previous            [1500]
-user_pref("mousewheel.transaction.timeout", 1500);
-// NSS     0       lines vary with accel            [5]
-user_pref("mousewheel.min_line_scroll_amount", 0);
-// NSS     3       keyboard matches mwheel          [5]
-user_pref("toolkit.scrollbox.horizontalScrollDistance", 3);
-// NSS     3       keyboard matches mwheel          [3]
-user_pref("toolkit.scrollbox.verticalScrollDistance", 3);
-// NSS  true          smoothness boost           [true]
-user_pref("layers.async-pan-zoom.enabled", true);
-// NSS  true          keyboard fix I            [false]
-user_pref("apz.force_disable_desktop_zooming_scrollbars", true);
-// NSS  false         keyboard fix II v85        [true]
-user_pref("apz.paint_skipping.enabled", false);
-// NSS  true       report wheel in pixels       [false]
-user_pref("dom.event.wheel-deltaMode-lines.always-disabled", true);
-// NSS  "300"      css mimics gecko scroll        [250]
-user_pref("layout.css.scroll-behavior.spring-constant", "300.0");
-// NSS   100      lame pref wreks settings        [100]
-user_pref("general.smoothScroll.mouseWheel.migrationPercent", 100);
-// NSS  false        no stutering fling         [false]
-// I like this to be true so that we stop scrolling as soon as we let go.
-user_pref("general.smoothScroll.msdPhysics.enabled", true);
-// NSS  "0.0"          reduce stutter            [0.25]
-user_pref("general.smoothScroll.currentVelocityWeighting", "0.0");
-// NSS   400           reduce stutter             [200]
-user_pref("general.smoothScroll.durationToIntervalRatio", 400);
-// NSS  "0.0"          reduce stutter             [0.4]
-user_pref("general.smoothScroll.stopDecelerationWeighting", "0.0");
-// NSS   300          arrows smoothing            [150]
-user_pref("general.smoothScroll.lines.durationMaxMS", 300);
-// NSS   300          arrows smoothing            [150]
-user_pref("general.smoothScroll.lines.durationMinMS", 300);
-// NSS   300          mwheel smoothing            [200]
-user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 300);
-// NSS   300          mwheel smoothing             [50]
-user_pref("general.smoothScroll.mouseWheel.durationMinMS", 300);
-// NSS   150          home-end smoothing          [150]
-user_pref("general.smoothScroll.other.durationMaxMS", 150);
-// NSS   150          home-end smoothing          [150]
-user_pref("general.smoothScroll.other.durationMinMS", 150);
-// NSS   200         pgup-pgdn smoothing          [150]
-user_pref("general.smoothScroll.pages.durationMaxMS", 200);
-// NSS   200         pgup-pgdn smoothing          [150]
-user_pref("general.smoothScroll.pages.durationMinMS", 200);
-// NSS   300           pixel smoothing            [150]
-user_pref("general.smoothScroll.pixels.durationMaxMS", 300);
-// NSS   300           pixel smoothing            [150]
-user_pref("general.smoothScroll.pixels.durationMinMS", 300);
-// NSS   500         scrollbar smoothing          [150]
-user_pref("general.smoothScroll.scrollbars.durationMaxMS", 500);
-// NSS   500         scrollbar smoothing          [150]
-user_pref("general.smoothScroll.scrollbars.durationMinMS", 500);
+///  NATURAL SMOOTH SCROLLING V4 "SHARP" - AveYo, 2020-2022             preset     [default]
+///  https://github.com/AveYo/fox
+///  copy into firefox/librewolf profile as user.js, add to existing, or set in about:config
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS",   12);//NSS    [120]
+user_pref("general.smoothScroll.msdPhysics.enabled",                    true);//NSS  [false]
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant",   200);//NSS   [1250]
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",       250);//NSS   [1000]
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",           25);//NSS     [12]
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",     "2.0");//NSS    [1.3]
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",      250);//NSS   [2000]
+user_pref("general.smoothScroll.currentVelocityWeighting",             "1.0");//NSS ["0.25"]
+user_pref("general.smoothScroll.stopDecelerationWeighting",            "1.0");//NSS  ["0.4"]
+
+/// adjust multiply factor for mousewheel - or set to false if scrolling is way too fast
+user_pref("mousewheel.system_scroll_override.horizontal.factor",         200);//NSS    [200]
+user_pref("mousewheel.system_scroll_override.vertical.factor",           200);//NSS    [200]
+user_pref("mousewheel.system_scroll_override_on_root_content.enabled",  true);//NSS   [true]
+user_pref("mousewheel.system_scroll_override.enabled",                  true);//NSS   [true]
+
+/// adjust pixels at a time count for mousewheel - cant do more than a page at once if <100
+user_pref("mousewheel.default.delta_multiplier_x",                       100);//NSS    [100]
+user_pref("mousewheel.default.delta_multiplier_y",                       100);//NSS    [100]
+user_pref("mousewheel.default.delta_multiplier_z",                       100);//NSS    [100]
+
+///  this preset will reset couple extra variables for consistency
+user_pref("apz.allow_zooming",                                          true);//NSS   [true]
+user_pref("apz.force_disable_desktop_zooming_scrollbars",              false);//NSS  [false]
+user_pref("apz.paint_skipping.enabled",                                 true);//NSS   [true]
+user_pref("apz.windows.use_direct_manipulation",                        true);//NSS   [true]
+user_pref("dom.event.wheel-deltaMode-lines.always-disabled",           false);//NSS  [false]
+user_pref("general.smoothScroll.durationToIntervalRatio",                200);//NSS    [200]
+user_pref("general.smoothScroll.lines.durationMaxMS",                    150);//NSS    [150]
+user_pref("general.smoothScroll.lines.durationMinMS",                    150);//NSS    [150]
+user_pref("general.smoothScroll.other.durationMaxMS",                    150);//NSS    [150]
+user_pref("general.smoothScroll.other.durationMinMS",                    150);//NSS    [150]
+user_pref("general.smoothScroll.pages.durationMaxMS",                    150);//NSS    [150]
+user_pref("general.smoothScroll.pages.durationMinMS",                    150);//NSS    [150]
+user_pref("general.smoothScroll.pixels.durationMaxMS",                   150);//NSS    [150]
+user_pref("general.smoothScroll.pixels.durationMinMS",                   150);//NSS    [150]
+user_pref("general.smoothScroll.scrollbars.durationMaxMS",               150);//NSS    [150]
+user_pref("general.smoothScroll.scrollbars.durationMinMS",               150);//NSS    [150]
+user_pref("general.smoothScroll.mouseWheel.durationMaxMS",               200);//NSS    [200]
+user_pref("general.smoothScroll.mouseWheel.durationMinMS",                50);//NSS     [50]
+user_pref("layers.async-pan-zoom.enabled",                              true);//NSS   [true]
+user_pref("layout.css.scroll-behavior.spring-constant",                "250");//NSS    [250]
+user_pref("mousewheel.transaction.timeout",                             1500);//NSS   [1500]
+user_pref("mousewheel.acceleration.factor",                               10);//NSS     [10]
+user_pref("mousewheel.acceleration.start",                                -1);//NSS     [-1]
+user_pref("mousewheel.min_line_scroll_amount",                             5);//NSS      [5]
+user_pref("toolkit.scrollbox.horizontalScrollDistance",                    5);//NSS      [5]
+user_pref("toolkit.scrollbox.verticalScrollDistance",                      3);//NSS      [3]
