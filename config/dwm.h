78689bd8075b4a711594f262204a126139bea36c
Koopa <lucy.loerker@gmail.com>
2022-12-15 18:06:55 -0500

    dwm: change default modkey function to tag
    We tag windows in/out more often than we reassign tags.

--- config/dwm.h
+++ config/dwm.h
@@ -71,8 +71,8 @@ static const Layout layouts[] = {
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY,                       KEY,      toggleview,   {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      view,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
