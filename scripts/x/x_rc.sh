4bde012b42abe884954feb08872c2cd1686ed24c
Koopa <lucy.loerker@gmail.com>
2022-12-28 16:06:20 -0500

    x_rc: add setxkbmap notes

--- scripts/x/x_rc.sh
+++ scripts/x/x_rc.sh
@@ -18,6 +18,21 @@ if [[ $CO_HOST = "DESKTOP" ]]; then
   nvidia-settings --load-config-only
 fi
 
+# Though I really try to use Xorg configuration files wherever possible, I just could not make it
+# work here. I faithfully made an equivalent configuration, and also had systemd-localed confirm
+# that I am doing it right, but it simply would not take effect. [1]
+#
+# I also tried pre-compiling the XKB keymap by using `setxkbmap … -print > xorg.xkb`, and loading
+# it here using `xkbcomp xorg.xkb "$DISPLAY"`. But, no matter *how* late in this file I do it, that
+# also does not work. If I run it from Konsole after starting dwm, though, then it works just fine.
+# In either case, it produces some errors that don't seem to be relevant.
+#
+# So, we have this: running setxkbmap every time. On the bright side, this is a different beast than
+# running `xrandr` at launch: there is no mode setting or anything. Just compiling the keymap and
+# applying it to the X server.
+#
+# [1] https://gist.github.com/CodingKoopa/60464ae2ed13b434c8b6cdfa909e811f
+
 # Set environment variables for graphical programs. Applcations started from dwm inherit its
 # environment, so this must be done prior to launching it. Non-graphical environment variables are
 # set earlier; see docs/Init.md.
@@ -37,6 +52,10 @@ while true; do
     source "$CO"/scripts/bash/user_graphical_post_rc.sh
     first_time=false
   fi
+  setxkbmap \
+    -layout "us,us" \
+    -variant ",dvp" \
+    -option "grp:alts_toggle,compose:lctrl,ctrl:nocaps" &
   fg "$(jobs |
     sed --quiet --regexp-extended "s/^\[([[:digit:]]+)\].+\bdwm\b.+$/\1/p" | head --lines=1)"
 done
