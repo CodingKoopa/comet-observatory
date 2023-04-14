084fafbdb9882759ea25cc69f5b319d2e894723f
Koopa <lucy.loerker@gmail.com>
2023-04-13 20:51:53 -0400

    config: add fcitx5

--- scripts/bash/setup_user_utils.sh
+++ scripts/bash/setup_user_utils.sh
@@ -57,6 +57,7 @@ function link_directories() {
     ["$CO/scripts/bash/bash_profile.sh"]="$INSTALL_HOME/.bash_profile"
     ["$CO/scripts/bash/bash_rc.sh"]="$INSTALL_HOME/.bashrc"
     ["$CO/scripts/x/x_rc.sh"]="$INSTALL_HOME/.xinitrc"
+    ["$CO/config/fcitx5"]="$INSTALL_HOME/.config/fcitx5"
 
     # Tools
 
