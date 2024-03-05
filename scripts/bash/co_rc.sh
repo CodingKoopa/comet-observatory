62b10a42b9f24f8e628720d58c17ab03d0757d13
Koopa <codingkoopa@gmail.com>
2024-03-05 02:31:01 -0500

    scripts/bash: recognize fw

--- scripts/bash/co_rc.sh
+++ scripts/bash/co_rc.sh
@@ -8,9 +8,9 @@ if [[ ! -d $CO ]]; then
 fi
 
 # Set the host.
-product_version=$(cat /sys/devices/virtual/dmi/id/product_version)
-readonly product_version
-if [[ $product_version = *P500* ]]; then
+if [[ $(cat /sys/devices/virtual/dmi/id/board_vendor) = Framework ]]; then
+  export CO_HOST=LAPTOP_FRAMEWORK
+elif [[ $(cat /sys/devices/virtual/dmi/id/product_version) = *P500* ]]; then
   export CO_HOST=LAPTOP_P500
 else
   export CO_HOST=DESKTOP
