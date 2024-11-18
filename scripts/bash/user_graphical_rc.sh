c5194c5e3282c0068c8d1c3f1f18f8bc7d9ebe44
Koopa <codingkoopa@gmail.com>
2024-11-18 18:09:58 -0500

    fcitx: remove

--- scripts/bash/user_graphical_rc.sh
+++ scripts/bash/user_graphical_rc.sh
@@ -16,14 +16,6 @@ export DESKTOP_SESSION=plasma
 export XCOMPOSEFILE=$CO/config/compose-keys.conf
 # Use the XDG portal for GTK applications.
 export GTK_USE_PORTAL=1
-# Use the fcitx input method for Qt applications.
-export QT_IM_MODULE=fcitx
-# Use the fcitx input method for GTK applications.
-export GTK_IM_MODULE=fcitx
-# Use the fcitx input method for certain SDL2 applications.
-export SDL_IM_MODULE=fcitx
-# Use the fcitx input method for XIM.
-export XMODIFIERS=@im=fcitx
 # We are using a non-reparenting WM.
 export _JAVA_AWT_WM_NONREPARENTING=1
 # Force anti-aliasing in Java (necessary for JFLAP).
